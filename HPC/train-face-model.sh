006c135b65f3236d7da272e38822ea0f2e7d9c9d
Nguyen Manh Tan <tan@Nguyens-MacBook-Pro.local>
2023-05-08 14:36:00 +0200

    update requirement file

--- HPC/train-face-model.sh
+++ HPC/train-face-model.sh
@@ -9,7 +9,8 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
 
-module load 2022r2
-module load cuda/11.6
+module load 2022r2 py-pip py-torch py-tqdm py-matplotlib py-pyyaml
+
+pip install opencv-python
 
 srun ./train.py --data ../../../../scratch/mnguyen1/MPIFaceGazeData --type face --epochs 20
