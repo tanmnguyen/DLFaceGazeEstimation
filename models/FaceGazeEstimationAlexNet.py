3ec53714831d1be3ef97f150d8e528d8f1937344
Nguyen Manh Tan <tan@Nguyens-MacBook-Pro.local>
2023-05-09 00:15:51 +0200

    update fc layer

--- models/FaceGazeEstimationAlexNet.py
+++ models/FaceGazeEstimationAlexNet.py
@@ -53,7 +53,7 @@ class AlexNetRegrModel(nn.Module):
             # nn.ReLU(inplace=True),
             # nn.Dropout(0.85),
             
-            nn.Linear(in_features=4096, out_features=2),
+            nn.Linear(in_features=2048, out_features=2),
         )
 
     def forward(self, x):
