6fd99aa1c7dea20f59f07d691be6a99e32addb8e
Nguyen Manh Tan <tan@Nguyens-MacBook-Pro.local>
2023-05-15 23:34:25 +0200

    add visual interpretation using fullgrad

--- interpret.py
+++ interpret.py
@@ -6,60 +6,50 @@ from utils.file import load_model
 from utils.runtime import available_device
 
 from torchvision import models, transforms
-
-
-from omnixai.data.image import Image
-from omnixai.preprocessing.image import Resize
-from omnixai.explainers.vision import VisionExplainer
-from omnixai.visualization.dashboard import Dashboard
-from omnixai.explainers.vision.specific.gradcam.pytorch.gradcam import GradCAM
-
 from datasets.FaceDataset import FaceDataset
+
+from pytorch_grad_cam import GradCAM, FullGrad
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget, RawScoresOutputTarget
+
+class RegressionOutputTarget:
+    def __init__(self):
+        pass
+
+    def __call__(self, model_output):
+        return torch.sum(model_output)
 
 def main(args):
     # load pretrained model
     _type, model = load_model(args.model)
-    model = model.to(available_device())
+    model = model.to(available_device()).eval()
 
     # read image 
-    img = cv2.imread(args.image)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = Resize((224, 224)).transform(Image(img))
+    rgb_img = cv2.cvtColor(cv2.imread(args.image), cv2.COLOR_BGR2RGB)
+    rgb_img = rgb_img.astype(np.float32) / 255.0
+
+    # convert to tensor 
+    input_tensor = torch.Tensor(rgb_img).float().permute(2,0,1)
+    input_tensor = input_tensor.unsqueeze(0)
+
+    # Construct the CAM object once, and then re-use it on many images:
+    cam = FullGrad(model=model, target_layers=[model.model.layer4[-1]], use_cuda=False)
+
+    targets = [RegressionOutputTarget()]
+
+    # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+    grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
+
+    # grayscale_cam = np.transpose(grayscale_cam, (1, 2, 0))[:, :, 0]
+    # cv2.imshow("GradCam", grayscale_cam)
+    # cv2.waitKey(0)
     
-    img = Image(
-        data=np.concatenate([
-            img.to_numpy()
-        ]),
-        batched=True
-    )
+    # In this example grayscale_cam has only one image in the batch:
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=False)
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    preprocess = lambda ims: torch.stack([transform(im.to_pil()) for im in ims])
-
-    explainer = VisionExplainer(
-        explainers=["gradcam"],
-        mode="regression",
-        model=model,                   
-        preprocess=preprocess,        
-        params={
-            # Set the target layer for GradCAM
-            "gradcam": {"target_layer": model.model.layer4[-1]},
-        },
-    )
-
-    # Generate explanations 
-    local_explanations = explainer.explain(img)
-
-    # Launch the dashboard
-    dashboard = Dashboard(
-        instances=img,
-        local_explanations=local_explanations,
-    )
-
-    dashboard.show()
+    cv2.imshow("GradCam", visualization)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
